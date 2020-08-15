a338197cf55f0b86859c57dfb6d36251e59d940e
Your Name <cighsen02@163.com>
2020-08-15 10:01:40 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="container">
+		<img src="./assets/images/logo.png">
+		<h1>Hello Vue 3!</h1>
+		<br />
+		<button class="btn btn-primary" @click="inc">Clicked {{ count }} times.</button>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+	setup() {
+		const count = ref(0)
+		const inc = () => {
+			count.value++
+		}
+
+		// effect(function log(){
+		// 	console.log('count changed!', count.value)
+		// })
+				
+		return {
+			count,
+			inc
+		}
+	}
+}
+</script>
+
+<style>
+	body {
+		text-align: center;
+	}
+	img {
+	  width: 200px;
+	}
+	h1 {
+	  font-family: Arial, Helvetica, sans-serif;
+	}
+</style>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { Buttons, Tooltip, Toast } from 'bootstrap';
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/scss/_custom.scss'
+//import '../node_modules/bootstrap/scss/bootstrap.scss'
+
+createApp(App).mount('#app')
